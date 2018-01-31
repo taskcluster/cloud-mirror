a262a6687b4480fe73f96c4230ba5ba30a600937
John Ford <john@johnford.org>
2018-01-31 14:47:59 +0100

    Disable sentry and statsum while debugging auth issue

--- src/main.js
+++ src/main.js
@@ -98,7 +98,7 @@ let load = base.loader({
     setup: ({process, profile, cfg}) => monitoring({
       project: 'cloud-mirror',
       credentials: cfg.taskcluster.credentials,
-      mock: profile !== 'production',
+      mock: true,//profile !== 'production',
       process,
     }),
   },
